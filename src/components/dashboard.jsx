cd526071bac3c3f698e305c403cdd3b67471b0a1
Kuzey <keyildiz29@my.aci.k12.tr>
2025-10-18 22:09:57 +0300

    fix

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -15,8 +15,8 @@ const Map = ({ onMapReady }) => {
       center: [27.138, 38.4192], // İzmir city center
       zoom: 11.5,
       maxBounds: [
-        [25.9, 37.6], // Southwest limit
-        [28.4, 39.2], // Northeast limit
+        [25.6, 37.6], // Southwest limit
+        [28.4, 39.5], // Northeast limit
       ],
     });
 
