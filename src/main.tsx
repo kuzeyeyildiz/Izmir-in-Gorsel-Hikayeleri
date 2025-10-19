481593c9013533375c31738963089a07541cd526
Kuzey <keyildiz29@my.aci.k12.tr>
2025-10-19 10:50:16 +0300

    m

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "maplibre-gl/dist/maplibre-gl.css";
+// @ts-ignore: no declaration file for './router'
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
 
